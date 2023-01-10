96ee631f074f168441bba15dc40b49802abd66ad
Shidqi <rifatpangestu87@gmail.com>
2023-01-10 14:18:47 +0700

    feat: add husky to check code before commit

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div className="grid place-content-center h-screen bg-slate-100">
-      <h1 className="text-black-700 font-semibold leading-5 text-4xl">Hello World!</h1>
+      <h1 className="text-sky-700 font-semibold leading-5 text-4xl">Hello World!</h1>
     </div>
   );
 }
