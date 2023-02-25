482a30ffed84f4b819446fffd8ae8bd9b55a8380
Shidqi <rifatpangestu87@gmail.com>
2023-02-25 17:50:11 +0700

    feat: setup project and provide multi language

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css';
+import 'styles/globals.css';
+import { useRouter } from 'next/router';
+import i18n from 'config/i18n';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  if (router.locale) {
+    i18n.changeLanguage(router.locale);
+  }
+
+  return <Component {...pageProps} __={i18n.t} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Home() {
+export default function Home({ __ }) {
   return (
     <div className="grid place-content-center h-screen bg-slate-100">
-      <h1 className="text-sky-700 font-semibold leading-5 text-4xl">Hello World!</h1>
+      <h1 className="text-sky-700 font-semibold leading-5 text-4xl">{__('hello')} World!</h1>
     </div>
   );
 }
