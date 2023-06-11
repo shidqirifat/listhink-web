63354b5f3691a3ab9947b6350fe0a78e12c3a07a
Shidqi <rifatpangestu87@gmail.com>
2023-06-11 20:07:54 +0700

    feat: styling pagination flow process for mobile

--- components/home/FlowProcess.js
+++ components/home/FlowProcess.js
@@ -107,7 +107,8 @@ export default function FlowProcess() {
           navigation
           pagination={{
             clickable: true,
-            bulletClass: 'bg-green-secondary w-4 h-4 rounded-full inline-block mx-2 cursor-pointer',
+            bulletClass:
+              'bg-green-secondary w-3 h-3 md:w-4 md:h-4 rounded-full inline-block mx-1 md:mx-2 cursor-pointer',
             bulletActiveClass: '!bg-green-primary'
           }}
           scrollbar={{ draggable: true }}
